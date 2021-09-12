1a119ae6be8f8cc0f3bbcea07a2ff1494fdaf665
Vlad Voinov <silveraquaris771002@gmail.com>
2021-09-12 20:19:00 +0300

    upd

--- ClassTask/ClassTask/Program.cs
+++ ClassTask/ClassTask/Program.cs
@@ -16,7 +16,43 @@ namespace ClassTask
             valera.InfoTeacher();
             valera.RemoveStudent(pasha);
             valera.InfoTeacher();
-            
+            Console.WriteLine("Choose what to do:\n " +
+                "1. Add student \n" +
+                "2." +
+                "3." +
+                "4." +
+                "5." +
+                "6." +
+                "7.");
+            int control = int.Parse(Console.ReadLine());
+            while (control != 0)
+            {
+                switch (control)
+                {
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+
+                }
+            }
         }
     }
 }
