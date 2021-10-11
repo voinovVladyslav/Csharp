6d9f926f09ecf0b0245eb2a56883fbb24490947a
Владислав <silveraquaris771002@gmail.com>
2021-10-11 14:50:36 +0300

    upd

---
+++ WinFormsTeachers/WinFormsTeachers/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace WinFormsTeachers
+{
+    partial class Application
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(129, 336);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 0;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NextButton);
+            this.Name = "Application";
+            this.Text = "WinApplication";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NextButton;
+    }
+}
+
